da92bdad9499c7557b34467dfad69d1f788cafef
HEEJAERO <rhj0830@gmail.com>
2023-04-18 22:49:44 +0900

    2023.04.18 코딩테스트 연습

--- src/프로그래머스/day2/연속부분수열합의개수.java
+++ src/프로그래머스/day2/연속부분수열합의개수.java
@@ -1,8 +1,24 @@
 package 프로그래머스.day2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class 연속부분수열합의개수 {
     public static int solution(int[] elements) {
-        int answer = 0;
+        // 3중 포문
+        Set<Integer> set = new HashSet<>();
+        int n = elements.length;
+        int tmp;
+        for(int i=0;i<n;i++){ // 원소를 몇개 뽑을꺼임?
+            for(int j=0;j<n;j++){ // 시작점
+                tmp = 0;
+                for(int k=0;k<i;k++){
+                    tmp += elements[(j+k)%n];
+                }
+                set.add(tmp); // 합
+            }
+        }
+        int answer = set.size();
         return answer;
     }
     public static void main(String[] args) {
