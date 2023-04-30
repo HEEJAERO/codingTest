77a6d0be0679e6f35fa4bda2f20c9adf6ffe5465
HEEJAERO <rhj0830@gmail.com>
2023-05-01 00:53:04 +0900

    2023.04.30 코딩테스트 연습

---
+++ src/프로그래머스/day10/소수찾기.java
@@ -0,0 +1,51 @@
+package 프로그래머스.day10;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class 소수찾기 {
+    // Level 2 -  dfs(재귀)
+    static boolean[] visited;
+    char[] nums;
+    static Set<Integer> set = new LinkedHashSet<>();
+    static StringBuilder s = new StringBuilder();
+    public int solution(String numbers) {
+
+        nums = numbers.toCharArray();
+        visited = new boolean[nums.length];
+        for(int i=0;i<nums.length;i++){
+            visited[i] = true;
+            dfs(nums[i],0);
+            visited[i] = false;
+        }
+        for(int a: set) System.out.print(a + " ");
+        return set.size();
+    }
+    void dfs(char num, int seq){
+       // System.out.println(s);
+        s.append(num);
+        isPrime(s.toString());
+        for(int i=0;i<visited.length;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(nums[i], seq + 1);
+                visited[i] = false;
+            }
+        }
+        s.deleteCharAt(seq);
+    }
+
+    void isPrime(String str){
+        int num = Integer.parseInt(str);
+        if(num==1 || num==0) return;
+        for(int i=2;i<=(int)Math.sqrt(num);i++){
+            if(num%i == 0) return;
+        }
+        set.add(num);
+    }
+
+    public static void main(String[] args) {
+        소수찾기 a = new 소수찾기();
+        System.out.println(a.solution("17"));
+    }
+}
---
+++ src/프로그래머스/day10/쿼드압축후세기.java
@@ -0,0 +1,38 @@
+package 프로그래머스.day10;
+
+public class 쿼드압축후세기 {
+    // Level 2 - 재귀
+    static int zero = 0;
+    static int one = 0;
+    public int[] solution(int[][] arr) {
+        int[] answer = {};
+        int n = arr.length;
+        compress(n,0,0,arr);
+        return new int[]{zero,one};
+    }
+    void compress(int n,int y, int x, int[][] arr){
+        //System.out.println(n);
+        int goal = arr[y][x];
+        //boolean flag = true; // 쿼드압축을 할 수 잇는지 확인
+        for(int i=y;i<y+n;i++){
+            for(int j=x;j<x+n;j++){
+                if(goal!=arr[i][j]) {
+                    compress(n/2,y,x,arr);
+                    compress(n/2,y+n/2,x,arr);
+                    compress(n/2,y,x+n/2,arr);
+                    compress(n/2,y+n/2,x+n/2,arr);
+                    return;
+                }
+            }
+        }
+        if(goal == 1) one ++;
+        else zero++;
+    }
+    public static void main(String[] args) {
+        쿼드압축후세기 a = new 쿼드압축후세기();
+        for (int b : a.solution(new int[][]{{1,1,0,0},{1,0,0,0},{1,0,0,1},{1,1,1,1}})) {
+            System.out.print(b + " ");
+        }
+
+    }
+}
